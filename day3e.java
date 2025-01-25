9d84d629ecfc4ac24a6228c1c9a5e25720afb819
monaksh <bhatnagar.monaksh2023@gmail.com>
2025-01-26 03:49:19 +0530

    Add files via upload

---
+++ day3e.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day3e{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        for (int i=1; i<11; i++){
+        System.out.println(i*n);
+        }
+ 
+    }
+}
---
+++ day5abook.java
@@ -0,0 +1,28 @@
+public class day5aBook{
+
+    String title;
+    String authorName;
+    int price;
+    String publisher;
+
+    public void checkDiscount(){
+    if (price >1000){
+        System.out.println("10 % OFF");
+    }
+        else{
+          System.out.println("5 % OFF");
+        }
+    }
+    
+}
+public class main{
+    public static void main( String[] args ){
+        day5aBook book = new day5aBook();
+        book.title ="harry potter and the half blood prince";
+        book.authorName ="jk rowling";
+        book.price =1200;
+        book.publisher="Bloomsbury";
+        book.checkDiscount();
+    
+    }
+}
