c9bd717db1d22717b2abe19ac2d3a522a77856bf
monaksh <bhatnagar.monaksh2023@gmail.com>
2025-02-05 01:18:17 +0530

    Day 8

---
+++ Day8a.java
@@ -0,0 +1,11 @@
+public class Day8a{
+    public static void main(String[] args) {
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+        String city = args[2];
+
+        System.out.println("name: "+name);
+        System.out.println("age: "+age);
+        System.out.println("city: "+city);
+    }
+}
