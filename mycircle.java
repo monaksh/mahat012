6e6e616ec3b52095749bd63e6e876bb51c6b0cb9
monaksh <bhatnagar.monaksh2023@gmail.com>
2025-02-07 02:12:35 +0530

    Day 8

---
+++ mycircle.java
@@ -0,0 +1,12 @@
+package shapes;
+
+public class circle {
+    public static void main(String[] args) {
+        circle c1 = new circle();
+        c1.R(10.12);
+        c1.calculateArea();
+        c1.displayArea();
+
+    }
+
+}
